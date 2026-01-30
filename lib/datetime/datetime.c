66e838e636a69eba7c9e21b36523ba522503da70
Ivan Mikhailov <imihajlow@gmail.com>
2026-01-30 18:39:15 +0100

    [FW-537] Integrate utz library (#18)
    * Add utz submodule
    
    * datetime: Timestamp parsing and formatting
    
    * Update utz
    
    * Zero is positive
    
    * Update utz
    
    * Update utz
    
    * Use utz types in datetime
    
    * Apply format
    
    * Format timezone offset
    
    * Apply format
    
    * Fix timestamp format
    
    * updated utz submodule ptr
    
    ---------
    
    Co-authored-by: hedger <hedger@nanode.su>

--- lib/datetime/datetime.c
+++ lib/datetime/datetime.c
@@ -1,5 +1,6 @@
 #include "datetime.h"
 #include <furi.h>
+#include <ctype.h>
 
 #define TAG "DateTime"
 
@@ -15,30 +16,7 @@ static const uint8_t datetime_days_per_month[2][MONTHS_COUNT] = {
 
 static const uint16_t datetime_days_per_year[] = {365, 366};
 
-bool datetime_validate_datetime(DateTime* datetime) {
-    bool invalid = false;
-
-    invalid |= (datetime->millis > 999);
-    invalid |= (datetime->second > 59);
-    invalid |= (datetime->minute > 59);
-    invalid |= (datetime->hour > 23);
-
-    invalid |= (datetime->year < 2000);
-    invalid |= (datetime->year > 2099);
-
-    invalid |= (datetime->month == 0);
-    invalid |= (datetime->month > 12);
-
-    invalid |= (datetime->day == 0);
-    invalid |= (datetime->day > 31);
-
-    invalid |= (datetime->weekday == 0);
-    invalid |= (datetime->weekday > 7);
-
-    return !invalid;
-}
-
-time_t datetime_datetime_to_timestamp(DateTime* datetime) {
+time_t datetime_datetime_to_timestamp(const DateTime* datetime) {
     furi_check(datetime);
 
     time_t timestamp = 0;
@@ -62,7 +40,7 @@ time_t datetime_datetime_to_timestamp(DateTime* datetime) {
         timestamp += datetime_get_days_per_month(leap_year, m) * SECONDS_PER_DAY;
     }
 
-    timestamp += (datetime->day - 1) * SECONDS_PER_DAY;
+    timestamp += (datetime->dayofmonth - 1) * SECONDS_PER_DAY;
     timestamp += datetime->hour * SECONDS_PER_HOUR;
     timestamp += datetime->minute * SECONDS_PER_MINUTE;
     timestamp += datetime->second;
@@ -70,43 +48,40 @@ time_t datetime_datetime_to_timestamp(DateTime* datetime) {
     return timestamp;
 }
 
-time_t datetime_datetime_to_timestamp_ms(DateTime* datetime) {
-    time_t timestamp = datetime_datetime_to_timestamp(datetime);
+time_t datetime_datetime_to_timestamp_ms(const DateTimeMs* datetime) {
+    time_t timestamp = datetime_datetime_to_timestamp(&datetime->dt);
     return 1000 * timestamp + datetime->millis;
 }
 
-void datetime_timestamp_to_datetime(time_t timestamp, DateTime* datetime) {
-    furi_check(datetime);
-
+DateTime datetime_timestamp_to_datetime(time_t timestamp) {
     time_t days = timestamp / SECONDS_PER_DAY;
     time_t seconds_in_day = timestamp % SECONDS_PER_DAY;
 
-    datetime->year = EPOCH_START_YEAR;
-    datetime->weekday = ((days + 3) % 7) + 1;
+    uint16_t year = EPOCH_START_YEAR;
 
-    while(days >= datetime_get_days_per_year(datetime->year)) {
-        days -= datetime_get_days_per_year(datetime->year);
-        (datetime->year)++;
+    while(days >= datetime_get_days_per_year(year)) {
+        days -= datetime_get_days_per_year(year);
+        year++;
     }
 
-    datetime->month = 1;
-    while(days >=
-          datetime_get_days_per_month(datetime_is_leap_year(datetime->year), datetime->month)) {
-        days -=
-            datetime_get_days_per_month(datetime_is_leap_year(datetime->year), datetime->month);
-        (datetime->month)++;
+    uint8_t month = 1;
+    while(days >= datetime_get_days_per_month(datetime_is_leap_year(year), month)) {
+        days -= datetime_get_days_per_month(datetime_is_leap_year(year), month);
+        month++;
     }
 
-    datetime->day = days + 1;
-    datetime->hour = seconds_in_day / SECONDS_PER_HOUR;
-    datetime->minute = (seconds_in_day % SECONDS_PER_HOUR) / SECONDS_PER_MINUTE;
-    datetime->second = seconds_in_day % SECONDS_PER_MINUTE;
-    datetime->millis = 0;
+    uint16_t hour = seconds_in_day / SECONDS_PER_HOUR;
+    uint16_t minute = (seconds_in_day % SECONDS_PER_HOUR) / SECONDS_PER_MINUTE;
+    uint16_t second = seconds_in_day % SECONDS_PER_MINUTE;
+
+    return (DateTime){
+        .date = utz_date_init(year, month, days + 1),
+        .time = {.hour = hour, .minute = minute, .second = second}};
 }
 
-void datetime_timestamp_ms_to_datetime(time_t timestamp, DateTime* datetime) {
-    datetime_timestamp_to_datetime(timestamp / 1000, datetime);
-    datetime->millis = timestamp % 1000;
+DateTimeMs datetime_timestamp_ms_to_datetime(time_t timestamp) {
+    DateTime dt = datetime_timestamp_to_datetime(timestamp / 1000);
+    return (DateTimeMs){.dt = dt, .millis = timestamp % 1000};
 }
 
 uint16_t datetime_get_days_per_year(uint16_t year) {
@@ -119,4 +94,177 @@ bool datetime_is_leap_year(uint16_t year) {
 
 uint8_t datetime_get_days_per_month(bool leap_year, uint8_t month) {
     return datetime_days_per_month[leap_year ? 1 : 0][month - 1];
+}
+
+void datetime_format_offset(const utz_offset_t* offset, char* buf) {
+    char offset_sign = '+';
+    uint8_t offset_h = 0;
+    uint8_t offset_m = 0;
+    // In offset_t only hours can be negative, minutes are always positive.
+    // For example, offset of -1:15 (-75 min) will be encoded as {-2,45}.
+    if(offset->hours >= 0) {
+        offset_h = offset->hours;
+        offset_m = offset->minutes;
+    } else {
+        offset_sign = '-';
+        if(offset->minutes == 0) {
+            offset_h = -offset->hours;
+        } else {
+            offset_h = -offset->hours - 1;
+            offset_m = 60 - offset->minutes;
+        }
+    }
+    snprintf(buf, DATETIME_OFFSET_STR_LEN + 1, "%c%02hhu:%02hhu", offset_sign, offset_h, offset_m);
+}
+
+void datetime_format_timestamp(const LocalTime* lt, char* buf) {
+    char offset_buf[DATETIME_OFFSET_STR_LEN + 1];
+    datetime_format_offset(&lt->offset, offset_buf);
+    snprintf(
+        buf,
+        DATETIME_TIMESTAMP_STR_LEN + 1,
+        "%04hu-%02hhu-%02hhuT%02hhu:%02hhu:%02hhu%6s",
+        lt->dt.year,
+        lt->dt.month,
+        lt->dt.dayofmonth,
+        lt->dt.hour,
+        lt->dt.minute,
+        lt->dt.second,
+        offset_buf);
+}
+
+static bool parse_int(const char** str, size_t width, unsigned int* result) {
+    unsigned int r = 0;
+    while(width > 0 && **str) {
+        int c = **str;
+        if(isdigit(c)) {
+            r = r * 10 + c - '0';
+        } else {
+            return false;
+        }
+        width -= 1;
+        *str += 1;
+    }
+    *result = r;
+    return true;
+}
+
+/** Parse date in ISO 8601 format.
+ * Accepted: YYYY-MM-DD or YYYYMMDD
+ */
+static bool parse_date(const char** str, utz_date_t* result) {
+    unsigned int y = 0, m = 0, d = 0;
+    bool hyphens = false;
+
+    if(!parse_int(str, 4, &y)) {
+        return false;
+    }
+    if(**str == '-') {
+        hyphens = true;
+        *str += 1;
+    }
+    if(!parse_int(str, 2, &m)) {
+        return false;
+    }
+    if(**str == '-' && !hyphens) {
+        return false;
+    }
+    *str += hyphens ? 1 : 0;
+    if(!parse_int(str, 2, &d)) {
+        return false;
+    }
+    return utz_date_init_checked(y, m, d, result);
+}
+
+/** Parse time in ISO 8601 format.
+ * Accepted: Thh:mm:ss or Thhmmss
+ */
+static bool parse_time(const char** str, utz_time_t* result) {
+    unsigned int h = 0, m = 0, s = 0;
+
+    if(**str != 'T') {
+        return false;
+    }
+    *str += 1;
+
+    bool hyphens = false;
+
+    if(!parse_int(str, 2, &h)) {
+        return false;
+    }
+    if(**str == ':') {
+        hyphens = true;
+        *str += 1;
+    }
+    if(!parse_int(str, 2, &m)) {
+        return false;
+    }
+    if(**str == ':' && !hyphens) {
+        return false;
+    }
+    *str += hyphens ? 1 : 0;
+    if(!parse_int(str, 2, &s)) {
+        return false;
+    }
+
+    return utz_time_init_checked(h, m, s, result);
+}
+
+/** Parse timezone offset in ISO 8601 format.
+ * Accepted: Z, ±hh:mm, ±hhmm, ±hh
+ */
+static bool parse_offset(const char* str, utz_offset_t* result) {
+    bool negative = false;
+    unsigned int h = 0, m = 0;
+
+    if(*str == 'Z') {
+        // UTC
+        str += 1;
+    } else {
+        // sign
+        if(*str == '-') {
+            negative = true;
+        } else if(*str != '+') {
+            return false;
+        }
+        str += 1;
+
+        // hours
+        if(!parse_int(&str, 2, &h)) {
+            return false;
+        }
+        if(*str == ':') {
+            str += 1;
+        }
+        if(*str != 0) {
+            // minutes
+            if(!parse_int(&str, 2, &m)) {
+                return false;
+            }
+        }
+    }
+    if(*str == 0) {
+        *result = utz_offset_init(negative, h, m);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool datetime_parse_timestamp(const char* str, DateTime* result) {
+    utz_datetime_t dt;
+    if(!parse_date(&str, &dt.date)) {
+        return false;
+    }
+    if(!parse_time(&str, &dt.time)) {
+        return false;
+    }
+    utz_offset_t offset;
+    if(!parse_offset(str, &offset)) {
+        return false;
+    }
+
+    *result = utz_udatetime_sub(&dt, &offset);
+
+    return true;
 }
--- lib/datetime/datetime.h
+++ lib/datetime/datetime.h
@@ -3,31 +3,26 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <time.h>
+#include <utz/utz.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-typedef struct {
-    // Time
-    uint8_t hour; /**< Hour in 24H format: 0-23 */
-    uint8_t minute; /**< Minute: 0-59 */
-    uint8_t second; /**< Second: 0-59 */
-    uint16_t millis; /**< Millisecond: 0-999 */
-    // Date
-    uint8_t day; /**< Current day: 1-31 */
-    uint8_t month; /**< Current month: 1-12 */
-    uint16_t year; /**< Current year: 2000-2099 */
-    uint8_t weekday; /**< Current weekday: 1-7 */
-} DateTime;
+#define DATETIME_TIMESTAMP_STR_LEN 26
+#define DATETIME_OFFSET_STR_LEN    7
 
-/** Validate Date Time
- *
- * @param      datetime  The datetime to validate
- *
- * @return     { description_of_the_return_value }
- */
-bool datetime_validate_datetime(DateTime* datetime);
+typedef utz_datetime_t DateTime;
+
+typedef struct {
+    DateTime dt;
+    uint16_t millis; /**< Millisecond: 0-999 */
+} DateTimeMs;
+
+typedef struct {
+    DateTime dt; ///< Local time
+    utz_offset_t offset; ///< Offset from UTC
+} LocalTime;
 
 /** Convert DateTime to UNIX timestamp
  * 
@@ -37,7 +32,7 @@ bool datetime_validate_datetime(DateTime* datetime);
  *
  * @return     UNIX Timestamp in seconds from UNIX epoch start
  */
-time_t datetime_datetime_to_timestamp(DateTime* datetime);
+time_t datetime_datetime_to_timestamp(const DateTime* datetime);
 
 /** Convert DateTime to UNIX timestamp in milliseconds
  *
@@ -47,7 +42,7 @@ time_t datetime_datetime_to_timestamp(DateTime* datetime);
  *
  * @return     UNIX Timestamp in milliseconds from UNIX epoch start
  */
-time_t datetime_datetime_to_timestamp_ms(DateTime* datetime);
+time_t datetime_datetime_to_timestamp_ms(const DateTimeMs* datetime);
 
 /** Convert UNIX timestamp to DateTime
  *
@@ -56,7 +51,7 @@ time_t datetime_datetime_to_timestamp_ms(DateTime* datetime);
  * @param[in]  timestamp  UNIX Timestamp in seconds from UNIX epoch start
  * @param[out] datetime   The datetime (UTC)
  */
-void datetime_timestamp_to_datetime(time_t timestamp, DateTime* datetime);
+DateTime datetime_timestamp_to_datetime(time_t timestamp);
 
 /** Convert UNIX timestamp in milliseconds to DateTime
  *
@@ -65,7 +60,7 @@ void datetime_timestamp_to_datetime(time_t timestamp, DateTime* datetime);
  * @param[in]  timestamp  UNIX Timestamp in milliseconds from UNIX epoch start
  * @param[out] datetime   The datetime (UTC)
  */
-void datetime_timestamp_ms_to_datetime(time_t timestamp, DateTime* datetime);
+DateTimeMs datetime_timestamp_ms_to_datetime(time_t timestamp);
 
 /** Gets the number of days in the year according to the Gregorian calendar.
  *
@@ -90,6 +85,41 @@ bool datetime_is_leap_year(uint16_t year);
  * @return the number of days in the month
  */
 uint8_t datetime_get_days_per_month(bool leap_year, uint8_t month);
+
+/** Format ISO 8601 timestamp: YYYY-MM-DDThh:mm:ss±hh:mm
+ *
+ * @param[in] lt local time.
+ * @param[out] buf string buffer. Must hold at least (DATETIME_TIMESTAMP_STR_LEN+1) bytes.
+ */
+void datetime_format_timestamp(const LocalTime* lt, char* buf);
+
+/** Format timezone offset: ±hh:mm
+ *
+ * @param[in] offset offset
+ * @param[out] buf string buffer. Must hold at least (DATETIME_OFFSET_STR_LEN+1) bytes.
+ */
+void datetime_format_offset(const utz_offset_t* offset, char* buf);
+
+/** Parse ISO 8601 timestamp.
+ *
+ * Timestamp must be in following format:
+ *  [DATE][TIME][ZONE]
+ * The following DATE formats are supported:
+ *  YYYY-MM-DD
+ *  YYYYMMDD
+ * The following TIME formats are supported:
+ *  Thh:mm:ss
+ *  Thhmmss
+ * The following ZONE formats are supported:
+ *  Z - UTC
+ *  ±hh:mm
+ *  ±hhmm
+ *
+ * @param[in] str timestamp string.
+ * @param[out] result timestamp (UTC).
+ * @return true on success.
+ */
+bool datetime_parse_timestamp(const char* str, DateTime* result);
 
 #ifdef __cplusplus
 }
